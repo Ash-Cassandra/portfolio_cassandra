a99b48880429cf44979f84951ed531a2a34cacc9
Ash-Cassandra <c.larcheveque@orange.fr>
2023-08-17 00:30:56 +0200

    modification css font size

--- src/components/Collaps/Collaps.jsx
+++ src/components/Collaps/Collaps.jsx
@@ -1,12 +1,23 @@
-import React, {useState} from "react";
+import React, {useState} from "react"
+import threePoints from '../../assets/three_points.webp'
+import onePoint from '../../assets/one_point.webp'
 
-function Collaps() {
+function Collaps({title, children}) {
 
     const [isOpen, setIsOpen] = useState(false)
+
+    const toggleCollaps = () => {
+        setIsOpen(!isOpen);
+      };
+
     return (
-<div>
-    
-</div>
+        <div>
+            <button className='btnCollaps' onClick={toggleCollaps}>
+                {title}
+                {isOpen ? <img src={onePoint} alt='#' className="CollapsOnePt"/> : <img src={threePoints} alt='#' className="CollapsThreePts"/>}
+            </button>
+            {isOpen && children}    
+        </div>
     )
 }
 
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import BlurredBackground from '../../components/BlurredBackground/BlurredBackground';
-import PageTitle from '../../components/Title/Title';
-import Nav from '../../components/Nav/Nav';
+import React from 'react'
+import BlurredBackground from '../../components/BlurredBackground/BlurredBackground'
+import PageTitle from '../../components/Title/Title'
+import Nav from '../../components/Nav/Nav'
+import Collaps from '../../components/Collaps/Collaps'
+import dataServices from '../../Services.json'
+import './Services.css'
 
 function Services() {
     return(
@@ -9,6 +12,20 @@ function Services() {
             <BlurredBackground/>
             <PageTitle title='services'/>
             <Nav/>
+
+            <div className='servicesTitle'>
+                {dataServices.map((service, index) => (
+                    <Collaps key={index} title={service.title}>
+                        <ul className='servicesDescription'>
+                            {service.description.map((description, index) => (
+                                <li key={index}>
+                                    {description}
+                                </li>
+                            ))}
+                        </ul>
+                    </Collaps>
+                ))}
+            </div>    
         </div>
     )
 }
