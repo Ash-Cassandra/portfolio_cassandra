16c6cda4dd5a5df824d75ae3b0c9ec8fa89ab269
Ash-Cassandra <c.larcheveque@orange.fr>
2023-08-16 23:13:15 +0200

    création json Services

---
+++ src/components/Collaps/Collaps.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+function Collaps() {
+
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+<div>
+    
+</div>
+    )
+}
+
+export default Collaps
