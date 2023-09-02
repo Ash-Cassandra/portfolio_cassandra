1d6ce454305a5e72f5054ad71c4773c93037d1ab
Ash-Cassandra <c.larcheveque@orange.fr>
2023-09-02 23:25:53 +0200

    modifictaion affichage contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BlurredBackground from '../../components/BlurredBackground/BlurredBackground';
 import PageTitle from '../../components/Title/Title';
 import Nav from '../../components/Nav/Nav';
 import './Contact.css'
 
 
-//handleClick pour afficher le mail et le numero de téléphone
 
 function Contact() {
+
     return(
         <div>
             <BlurredBackground/>
@@ -15,9 +15,16 @@ function Contact() {
             <Nav/>
                 <main>
                     <adress className="contact">
-                            <p>Pour toute demande de devis ou pour repondre à vos questions, je suis là pour vous aider.</p>
-                                <a href="mailto:cassandra_devweb@outlook.fr" className='contactInfo'>Par mail</a><br/>
-                                <a href="tel:0675073509" className='contactInfo'>Par téléphone</a><br/>
+                            <p>Pour toute demande de devis ou pour tout renseignement ou question, contactez-moi.<br/>
+                            Chaque demande sera étudiée.</p>
+                                <a href="mailto:cassandra_devweb@outlook.fr" className='contactTitle'>
+                                    Par mail<br/>
+                                    <span className='contactInfo'>cassandra_devweb@outlook.fr</span>
+                                    </a>
+                                <a href="tel:0675073509" className='contactTitle' >
+                                    Par téléphone<br/>
+                                    <span className='contactInfo'>06.75.07.35.09</span>
+                                    </a>
                     </adress>
                 </main>
         </div>
