1cad8da35cc559ac8ff775020b397327c0693f00
Ash-Cassandra <c.larcheveque@orange.fr>
2023-09-11 22:26:37 +0200

    ajout json projects

--- src/pages/Portfolio/portfolio.jsx
+++ src/pages/Portfolio/portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BlurredBackground from '../../components/BlurredBackground/BlurredBackground';
 import PageTitle from '../../components/Title/Title';
 import Nav from '../../components/Nav/Nav';
+import dataProjects from '../../Projects.json'
 
 function Portfolio() {
     return(
@@ -10,7 +11,12 @@ function Portfolio() {
             <PageTitle title='portfolio'/>
             <Nav/>
             <main>
-                
+            {dataProjects.map((projects, index) => (
+                            <figure key={index} className='ProjectsCard'>
+                                <figcaption className='ProjectsTitle'>{projects.title}</figcaption>
+                                <img className='projectsImg' src={projects.cover} alt={projects.title} />
+                            </figure>
+                        ))}
             </main>
         </div>
     )
