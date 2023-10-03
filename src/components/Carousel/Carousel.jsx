bb540cd9c0af0bb5348a3fff009044f4df647b5f
Ash-Cassandra <c.larcheveque@orange.fr>
2023-10-03 11:02:46 +0200

    css main

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,35 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import './Carousel.css'
 
 
  function Carousel(props) {
     const [currentImg, setCurrentImg] = useState(0);
 
-        const handleNext = () => {
-            setCurrentImg ((prevImg) => (prevImg === props.children.length -1 ? 0 : prevImg + 1))
+        const handleIndicatorClick = (index) => {
+            setCurrentImg(index)
         }
 
-        const handlePrev = () => {
-            setCurrentImg ((prevImg) => (prevImg === 0 ? props.children.length - 1 : prevImg - 1))
-        }
+        const nextSlide = () => {
+            const newIndex = (currentImg + 1) % props.children.length;
+            setCurrentImg(newIndex);
+          };
 
-    
-    const index = currentImg + 1
-    const counter = props.children.length > 1 ? <p>{index} / {props.children.length} </p> : null
-    const buttons = props.children.length > 1 ? (
-            <div>
-                <button className='buttonCarousel btnCarouselRight' onClick={handleNext}>
-                </button> 
-                <button className='buttonCarousel btnCarouselLeft' onClick={handlePrev}>
-                </button>
-            </div>
-        ) : null
+          useEffect(() => {
+            const interval = setTimeout(nextSlide, 3000); // transition automatique 3 secondes
+        
+            return () => {
+              clearTimeout(interval);
+            };
+          }, [currentImg]);
+        
+
+        const indicator = props.children.map((_, index) => (
+            <div
+            key={index}
+            className={`carousel-indicator ${currentImg === index ? 'active-indicator' : ''}`}
+            onClick = {() => handleIndicatorClick(index)}
+            ></div>
+        ))
+
 
 return (
     <div className='divCarousel'>
         {props.children[currentImg]}
-            {buttons}
-        <div className='indexCurrentImg'>
-            {counter}
+        <div className='indicators'>
+            {indicator}
         </div>
     </div>  
     )
